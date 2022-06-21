86ecc939756953dcb96f4aefbfa0a45ea4817f0d
Andrew <andrew@hulkrelax.xyz>
2022-06-21 10:11:04 -0500

    remove unneeded comment

--- src/index.tsx
+++ src/index.tsx
@@ -3,5 +3,5 @@ import { App } from './components/App/App'
 
 import { createRoot } from 'react-dom/client'
 const container = document.getElementById('root')
-const root = createRoot(container) // createRoot(container!) if you use TypeScript
+const root = createRoot(container)
 root.render(<App />)
