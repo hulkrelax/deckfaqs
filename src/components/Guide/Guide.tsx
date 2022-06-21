9a6e9b1be865878dea3b41ae148d007cc25f48db
Andrew <andrew@hulkrelax.xyz>
2022-06-21 07:39:09 -0500

    initial commit of code

---
+++ src/components/Guide/Guide.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import styles from './Guide.module.css';
+
+type GuideProps = {
+    url: string;
+    text: string | undefined;
+}
+
+export const Guide = ({ url, text }: GuideProps) => (
+        text ? (<div className={styles.plain}>
+            <pre>{text}</pre>
+        </div>) :
+        (<iframe className={styles.guide} src={url} sandbox="" />)
+)
---
+++ src/components/List/List.tsx
@@ -0,0 +1,34 @@
+import React from "react";
+import { ListElement } from "../ListElement/ListElement";
+import styles from './List.module.css'
+
+export type ListItem = {
+    text: string;
+    url?: string | undefined;
+}
+
+interface ListProps {
+    header: string
+    data: ListItem[]
+    handleClick: (text: string) => void;
+}
+
+export const List = ({
+    data,
+    header,
+    handleClick
+}: ListProps) => {
+    return (
+        <>
+            <h1>{header}</h1>
+            <ul className={styles.mylist}>
+                {
+                    data?.map((item) => (
+                        <ListElement key={item.text} text={item.text} url={item.url} handleOnClick={handleClick} />
+                    ))
+                }
+            </ul>
+            
+        </>
+    )
+};
