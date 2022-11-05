95e86c70187796e264c755209216bc449297f044
Andrew <andrew@hulkrelax.xyz>
2022-11-05 10:20:40 -0500

    delete dead code

--- src/MyRouter.ts
+++
@@ -1,6 +0,0 @@
-export default interface MyRouter {
-    get MainRunningApp(): AppOverview | undefined;
-    NavigateToRunningApp(replace?: boolean): void;
-    CreateBrowserView(name: string): any;
-}
-export declare const MyRouter: MyRouter;
--- src/components/App/MyRouter.tsx
+++
@@ -1,9 +0,0 @@
-import { findModuleChild, Module } from 'decky-frontend-lib';
-import MyRouter from '../../MyRouter';
-
-export const MyRouter = findModuleChild((m: Module) => {
-    if (typeof m !== 'object') return undefined;
-    for (let prop in m) {
-        if (m[prop]?.Navigate && m[prop]?.NavigationManager) return m[prop];
-    }
-}) as MyRouter;
